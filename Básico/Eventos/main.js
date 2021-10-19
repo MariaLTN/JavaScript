701d9cf77e274c78e0e2bd6324820af50b4b083a
Maria <60711926+MariaLTN@users.noreply.github.com>
2021-10-19 18:22:39 -0300

    Add files via upload

---
+++ Básico/Eventos/main.js
@@ -0,0 +1,41 @@
+//Mouse
+
+function Clicar(){
+	alert('Você clicou UMA VEZ.');
+}
+
+function DBL(){
+	alert('Você clicou DUAS VEZES.')
+}
+
+function OVER(){
+	alert('Você PASSOU SOBRE o bloco.')
+}
+
+function OUT(){
+	alert('Você SAIU do bloco.')
+}
+
+// Teclado
+
+function Down(){
+	alert('Tecla Down Pressionada');
+}
+
+function Up(){
+	alert('Tecla Up Pressionada');
+}
+
+function Press(){
+	alert('Tecla Press Pressionada');
+}
+
+// Janela
+
+function Resize(){
+	alert('Resize Acontecendo');
+}
+
+function Scroll(){
+	alert('Scroll Acontecendo');
+}
