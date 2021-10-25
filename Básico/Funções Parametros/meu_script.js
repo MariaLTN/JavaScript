f75d4510827c5ee0d9ec98f97c906061a03db038
Maria <60711926+MariaLTN@users.noreply.github.com>
2021-10-25 18:19:13 -0300

    Update meu_script.js

--- Básico/Funções Parametros/meu_script.js
+++ Básico/Funções Parametros/meu_script.js
@@ -4,7 +4,13 @@ function soma (a,b) {
 	return a+b;
 }
 
+function soma_diferente (a=0,b=0) {
+	return a+b;
+}
+
 console.log(soma(7, 7));
 console.log(soma(7, 7, 9, 15));
 console.log(soma(7));
 console.log(soma());
+
+console.log(soma_diferente(7));
