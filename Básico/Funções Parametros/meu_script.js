1e011f403d7b78fcbb8bd5557b4dc7dadf244a24
Maria <60711926+MariaLTN@users.noreply.github.com>
2021-10-25 20:19:10 -0300

    Update meu_script.js

--- Básico/Funções Parametros/meu_script.js
+++ Básico/Funções Parametros/meu_script.js
@@ -27,3 +27,15 @@ console.log(fatorial(7)); //Resulta em 5040
 console.log(fatorial(5)); //Resulta em 120
 console.log(fatorial(1)); //Resulta em 1
 
+function soma_sem_param () {
+	var resultado = 0;
+	for(var i in arguments) {
+		resultado += arguments[i];
+	}
+	return resultado;
+}
+
+console.log(fatorial(7,-1,0.5)); //Resulta em 6.5
+console.log(fatorial(7,-1,0.5,"Amanda")); //Resulta em 6.5Amanda
+console.log(fatorial(7)); //Resulta em 7
+console.log(fatorial()); //Resulta em 0
