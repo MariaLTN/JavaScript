f59e9e30be4151d7dfc7c2de94d8e625e6393fc5
Maria <60711926+MariaLTN@users.noreply.github.com>
2021-11-11 17:16:50 -0300

    Update main.js

--- Básico/Funções construtoras/main.js
+++ Básico/Funções construtoras/main.js
@@ -1,19 +1,29 @@
 //Notação de Classe (ES6):
 //Notação de POO com o método Get e Set;
 
+//Usamos o método de encapsulamento, criando variáveis dentro de um escopo da função.
+
+//Observação: Funções também são estruturas de dados.
+
+
 let Carro = function(){
 	this.cor = "Amarelo";
 	this.modelo = "Ferrari";
 	this.velocidadeAtual = 0;
-	this.velocidadeMaxima = 60;
+
+	var velocidadeMaxima = 60; //Seria uma forma "privada"
+	//this.velocidadeMaxima = 60; // Assim ele está "público."
+	var quilometrosRodados = 0;
 
 	this.acelerar = function(){
-		if (this.getVelocidadeAtual() >= this.velocidadeMaxima) {
+		if (this.getVelocidadeAtual() >= /*this.*/velocidadeMaxima) {
 			console.log("Você está em uma velocidade muito alta, tenha cuidado.");
 			console.log("Não pode aumentar mais do que isso.")
-		} else if (this.getVelocidadeAtual() < this.velocidadeMaxima){
-			this.setVelocidadeAtual(this.getVelocidadeAtual() + 10);
+		} else if (this.getVelocidadeAtual() < /*this.*/velocidadeMaxima){
+			this.setVelocidadeAtual(this.getVelocidadeAtual() + 15);
 		}
+		setQuilometrosRodados();
+		//console.log(quilometrosRodados)
 	}
 
 	this.getVelocidadeAtual = function(){
@@ -23,16 +33,21 @@ let Carro = function(){
 	this.setVelocidadeAtual = function(velocidade){
 		this.velocidadeAtual = velocidade;
 	}
+
+	var setQuilometrosRodados = function(){ //Só funciona na função, 'encapsulamos' a função
+		quilometrosRodados += 0.5;
+		console.log(quilometrosRodados);
+	}
 }
 
 let carro = new Carro();
 
-carro.acelerar();  //10
-carro.acelerar();  //20
+carro.velocidadeMaxima = 200; //Já não influência mais o código.
+carro.acelerar();  //15
 carro.acelerar();  //30
-carro.acelerar();  //40
-carro.acelerar();  //50
+carro.acelerar();  //45
 carro.acelerar();  //60
-carro.acelerar();  //70 - Já não aumenta mais.
+carro.acelerar();  //75 - Já não aumenta mais.
+console.log(carro.velocidadeMaxima);
 
-console.log(carro.getVelocidadeAtual());
+console.log(carro.getVelocidadeAtual());;
